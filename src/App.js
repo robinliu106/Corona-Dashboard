232903faaf3fe8482450c2441cb121c49754b1a6
Robin Liu <robinliu106@gmail.com>
2020-06-30 22:22:48 -0400

    added chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
-};
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+    state = {
+        data: {},
+    };
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData });
+        // console.log(fetchedData);
+    }
+    render() {
+        const { data } = this.state;
+        return (
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
+}
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async () => {
+    try {
+        //destructure data
+        const {
+            data: { confirmed, recovered, deaths, lastUpdate },
+        } = await axios.get(url);
+
+        const modifiedData = {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate,
+        };
+
+        return modifiedData;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    } catch (error) {
+        console.log("fetch daily data error", error);
+    }
+};
