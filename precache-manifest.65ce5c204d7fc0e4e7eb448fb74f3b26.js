e40d776525d7d26c75580e92c915e40b6bf40222
Robin Liu <robinliu106@gmail.com>
2021-02-06 21:04:24 -0500

    Updates

---
+++ precache-manifest.65ce5c204d7fc0e4e7eb448fb74f3b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f180057d33dc518ae5ce27f085d259b7",
+    "url": "/Corona-Dashboard/index.html"
+  },
+  {
+    "revision": "405b9d5a48d27b4b234e",
+    "url": "/Corona-Dashboard/static/css/main.8247181c.chunk.css"
+  },
+  {
+    "revision": "6d66b3b69df24d950fa4",
+    "url": "/Corona-Dashboard/static/js/2.a4d72f3d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Dashboard/static/js/2.a4d72f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405b9d5a48d27b4b234e",
+    "url": "/Corona-Dashboard/static/js/main.cf3a02db.chunk.js"
+  },
+  {
+    "revision": "2913ea613a53786d9739",
+    "url": "/Corona-Dashboard/static/js/runtime-main.ec0a1e77.js"
+  }
+]);
